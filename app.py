e8ba44db41a84765b6432a1d28a712b076136557
Dominicod <dominicodonnell99@gmail.com>
2022-01-29 14:34:11 -0600

    Login landing page designed. Extremely temporary, no mobile deisgn yet implemented

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ from flask_session import Session
 
 app = Flask(__name__)
 
+# Makes sure templates are reloading when edited
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensures reponses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1 
+    response.headers["Expires"] = 0 # HTTP 1.0
+    response.headers["Pragma"] = "no-cache" # Proxies
+    return response
+
+# Main homepage of website
 @app.route("/")
 def index():
     return render_template("login.html")
