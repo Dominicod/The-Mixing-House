7fd1fcfe4b4d3fa69542862d017dc647e89ff1e0
Dominicod <dominicodonnell99@gmail.com>
2022-01-31 15:30:49 -0600

    Added image cycling to Background image

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,16 +1,32 @@
-
-// Will make this function correctly cycle through images at some point
-
-let BG1 = "url('/static/img/BG1.jpg')"
-let BG2 = "url('/static/img/BG2.jpg')"
-let BG3 = "url('/static/img/BG3.jpg')"
-let BG4 = "url('/static/img/BG4.jpg')"
-arr = [BG1, BG2, BG3, BG4];
-
-function ImageCycle() {
-    random = Math.floor(Math.random() * arr.length)
-    document.getElementById("hero").style.backgroundImage=arr[random];
+// Creates a class to define what a background image is
+class BGImg {
+    constructor(url, displayed) {
+        this.url = url;
+        this.displayed = displayed;
+    }
 }
 
-// Calls ImageCycle
-ImageCycle();
+// Assigns background images
+let BG1 = new BGImg("url('/static/img/BG1.jpg')", false)
+let BG2 = new BGImg("url('/static/img/BG2.jpg')", false)
+let BG3 = new BGImg("url('/static/img/BG3.jpg')", false)
+let BG4 = new BGImg("url('/static/img/BG4.jpg')", false)
+arr = [BG1, BG2, BG3, BG4];
+total = 0;
+
+// Cycles through the background images untill it cannot cycle anymore, which it then reevaluates arr.displayed = false
+window.onload = function ImageCycle() {
+    random = Math.floor(Math.random() * arr.length)
+    if (arr[random].displayed == false) {
+        document.getElementById("hero").style.backgroundImage=arr[random].url;
+        arr[random].displayed = true;
+        total++;
+    }
+    else if (total == arr.length) {
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].displayed = false;
+        }
+        total = 0;
+    }
+    timerRef = setTimeout(ImageCycle, 10000);
+}
